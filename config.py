2168763e82f8f3ef586b5949892df1c1f6842734
DeYang03 <163637244+DeYang03@users.noreply.github.com>
2024-04-16 15:47:18 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-2.c5meggg4gha2.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "leeteyang-employee"
+customregion = "us-east-1"
+
